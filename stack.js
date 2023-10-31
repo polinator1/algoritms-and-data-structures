e0df4417663b8637429b7acc806bc76996182981
polinator1 <shc-polina@yandex.ru>
2023-10-31 18:30:13 +0300

    added new files

---
+++ stack.js
@@ -0,0 +1,21 @@
+var letters = [] // this is our stack
+
+var word = "racecar"
+
+var rword = ""
+
+// put letters of word into the stack
+for (let i = 0; i < word.length; i++) {
+    letters.push(word[i])
+}
+
+// pop off the stack in reverse order
+for (let i = 0; i < word.length; i++) {
+    rword += letters.pop()
+}
+
+if (rword === word) {
+    console.log(word + " is a palindrome")
+} else {
+    console.log(word + " is not a palindrome")
+}
