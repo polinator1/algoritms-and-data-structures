ff8fb11e1dc102cadd845dddc9d99bfa945c0df2
polinator1 <shc-polina@yandex.ru>
2023-11-19 00:21:43 +0300

    refactored answer

--- yandex 1.0/3/3h.js
+++ yandex 1.0/3/3h.js
@@ -1,33 +1,18 @@
-function findSolution(data) {
+function getResult(data) {
 	let length = parseInt(data[0].trim());
 	let setX = new Set();
-    let counter = 0;
 	for (let i = 1; i <= length; i++) {
-		let x = data[i][0].trim().split(' ').map(input => parseInt(input));
-        let y = data[i][1].trim().split(' ').map(input => parseInt(input));
-        setX.add([x, y]);
-    }
-    
-    
-    for (let i = 0; i < setX.size; i++) {
-    	if (x[i] == x[i + 1] && y[i] > y [y + 1]) {
-                setX.delete([x[i + 1], y[i + 1]]);
-                setX.delete([x[i], y[i]]);
-                counter++;
-            } else {
-            	setX.delete([x[i], y[i]]);
-                counter++;
-            }
-    }
-    
-    return counter;
-    
+		let [x, y] = data[i].trim().split(' ').map(input => parseInt(input));
+		setX.add(x);
+	}
+
+	return setX.size;
 }
 
 const fs = require('fs');
 let fileContent = fs.readFileSync("input.txt", "utf8");
 let data = fileContent.toString().split("\n");
 
-const result = findSolution(data);
+const result = getResult(data);
 
 fs.writeFileSync("output.txt", result.toString());
